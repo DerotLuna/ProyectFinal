ed760d96173d28f8554755ecd9946b0e54998abc
morty <alemv96@hotmail.com>
2016-05-18 15:28:48 -0430

    Observación.
    ya se coloco la vecindad entre las casillas del tablero

--- Project-1010!/Board.java
+++ Project-1010!/Board.java
@@ -16,15 +16,49 @@ public class Board{
     for (byte counter = 0; counter < numberOfBoxes; counter++) { //no creo que se necesiten mas de 127 casillas, digo que el max sera 100.
       boxes[counter] = new Box(counter);
     }
-    neighborhood = new boolean[dimension][dimension];
-    for (byte counterRows = 0; counterRows < dimension; counterRows++) {
-      for (byte counterColumns = 0; counterColumns < dimension; counterColumns++) {
-        neighborhood[counterRows][counterColumns] = false;
+    neighborhood = new boolean[numberOfBoxes][numberOfBoxes];
+    for (int counterRows = 0; counterRows < numberOfBoxes; counterRows++) {
+      for (int counterColumns = 0; counterColumns < numberOfBoxes; counterColumns++) {
+          neighborhood[counterRows][counterColumns] = false;
       }
     }
   }
 
+  public void shapeBoard(){
+    byte rowAndColumn = 0;
+    byte counterStrike = dimension;
+    byte counter = (byte)(counterStrike - 1);
+    while (rowAndColumn < (numberOfBoxes - 1)){
+
+      if(rowAndColumn <= counter){
+        if (counter < numberOfBoxes - 1){
+          neighborhood[rowAndColumn][counterStrike] = true;
+          neighborhood[counterStrike][rowAndColumn] = true;
+        }
+        if (rowAndColumn < counter){
+          neighborhood[rowAndColumn + 1][rowAndColumn] = true;
+          neighborhood[rowAndColumn][rowAndColumn + 1] = true;
+        }
+      }
+      System.out.println("rowAndColumn " + rowAndColumn);
+      System.out.println("counterStrike " + counterStrike);
+      System.out.println("counter " + counter);
+      if (rowAndColumn == counter) counter = counterStrike;
+      if (counterStrike < numberOfBoxes - 1) counterStrike ++;
+      rowAndColumn ++;
+    }
+  }
+
+  public void printBoard(){
+    for (int counterRows = 0; counterRows < numberOfBoxes; counterRows++) {
+      for (int counterColumns = 0; counterColumns < numberOfBoxes; counterColumns++)
+          System.out.print("| " + neighborhood[counterRows][counterColumns] + " | ");
+      System.out.println();
+    }
+  }
+
   public void statusBox(){
+    printBoard();
     boxes[5].getStatus();
     boxes[8].setStatus(new LockedBox());
     boxes[8].getStatus();
