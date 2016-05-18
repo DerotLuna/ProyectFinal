1d84775c519bd1ed4a1540bc0d56d36e8ff1a9ed
morty <alemv96@hotmail.com>
2016-05-18 15:31:33 -0430

    Update World.java

--- Project-1010!/World.java
+++ Project-1010!/World.java
@@ -4,6 +4,8 @@ public class World{
     private int score;
     //private Menu subMenu; //atributo posible, o puede que sea un metodo!!!
     private Player player;
+    private Piece[] capsule;
+    private final byte range;
   /*Atributos tentativos:
     private Board board;
     private Capsule capsule;*/
@@ -11,6 +13,8 @@ public class World{
   public World(Player player){
     this.player = player;
     this.score = 0; //validar que el juego sigue o si es una partida nueva
+    this.range = 3;
+    this.capsule = new Piece[range];
   }
 
   /*Se puede pasar el usuario por aqui o hacer un setter para asi cambiarlo
@@ -22,11 +26,12 @@ public class World{
       siempre sera igual un almacenador, sin importar la forma del tablero. Incluso podria ponerlo de una vez en el
       constructor con su valores.
     */
-    byte dimension = 10; //lo que se vaya a determinar, igual se cambiara este parametro y el atributo a donde va
-    final byte dimensionCapsule = 5;
+    byte dimension = 3; //lo que se vaya a determinar, igual se cambiara este parametro y el atributo a donde va
     Board board = new Board(dimension);
-    Capsule capsule = new Capsule(dimensionCapsule);
-
+    board.shapeBoard();
+    for (byte counterOfPieces = 0; counterOfPieces < range; counterOfPieces ++) {
+      capsule[counterOfPieces] = new Piece();
+    }
     System.out.println("Jugador: " + player.getUser() + " Mejor puntaje: " + player.getBestScore() + " Puntaje actual: " + score);
     board.statusBox(); //pruebas
 
