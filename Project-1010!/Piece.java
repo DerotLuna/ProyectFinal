f127dd17f92e10f2eb3f0de18c8b7dc319c28506
Ruben Luna <rubenlunamate@gmail.com>
2016-05-20 22:26:24 -0430

    Cambio de atributos
    shapePiece por type, size. Esto fue para tener un mejor manejo, y que el manejo se pudiese dar algo mas general.

--- Project-1010!/Piece.java
+++ Project-1010!/Piece.java
@@ -1,106 +1,110 @@
 import java.util.Random;
 
 public class Piece{
-  private String shape;
-  private byte size;
+  private String type; // hay tres tipos en 1010: cuadrado, linea, l
+  private byte size; //la altura de la pieza (en l es la parte mas larga)
   private String color;
-  Random randomNumber = new Random();
+  private byte id;
   /*Atributo para saber que ocupa en Capsule o Board
     Se me ocurre hacer una clase padre entre tablero y capsula, donde compartan los atributos de casillas y numero de casillas.
     Al hacer eso, podre utilizar el polimorfismo para asi saber si la pieza pertene a tablero o a capsule. Esto es en caso
     de que sea necesario tener este tipo de dato, que creo que si.
   */
-
-  public Piece(){
-    byte shapeNumber = (byte)(randomNumber.nextInt()* 19 + 1);
-    byte color = (byte)(randomNumber.nextInt()* 6 + 1);
-    this.shape = shapePieces(shapeNumber);
+  Random randomNumber = new Random();
+  public Piece(int numberOfPieces){
+    int randomPiece = (int)(randomNumber.nextDouble() * numberOfPieces + 1);
+    int randomColor = (int)(randomNumber.nextDouble() * 6 + 1);
+    shapePieces(randomPiece);
+    //this.id = id;
+    //this.color = colors(randomColor);
   }
 
-  public String shapePieces(byte shapeNumber){
-    /*Esto me generaria las cosas random que necesite, modificar a un nombre mas
-      mnemonico */
+  public String getTypePiece(){
+    return this.type;
+  }
 
-      String shape;//polsia
+  public byte getSizePiece(){
+    return this.size;
+  }
 
-      if (shapeNumber == 1){
-        size = 1;
-        shape = "Cuadrado 1X1";
-      }
-      else if (shapeNumber == 2){
-        size = 4;//tiene 4 casillas por decirlo de una manera, esto puede funcionar para el score (si es que lo usamos asi)
-        shape = "Cuadrado 2X2";
-      }
-      else if (shapeNumber == 3){
-        size = 9;
-        shape = "Cuadrado 3X3";
-      }
-      else if (shapeNumber == 4){
-        size = 2;
-        shape = "linea horizontal de 2 casillas";
-      }
-      else if (shapeNumber == 5){
-        size = 3;
-        shape = "linea horizontal de 3 casillas";
-      }
-      else if (shapeNumber == 6){
-        size = 4;
-        shape = "linea horizontal de 4 casillas";
-      }
-      else if (shapeNumber == 7){
-        size = 5;
-        shape = "linea horizontal de 5 casillas";
-      }
-      else if (shapeNumber == 8){
-        size = 2;
-        shape = "Linea vertical de 2 casillas";
-      }
-      else if (shapeNumber == 9){
-        size = 3;
-        shape = "linea vertical de 3 casillas";
-      }
-      else if (shapeNumber == 10){
-        size = 4;
-        shape = "linea vertical de 4 casillas";
-      }
-      else if (shapeNumber == 11){
-        size = 5;
-        shape = "linea vertical de 5 casillas";
-      }
-      else if (shapeNumber == 12){
-        size = 3;
-        shape = "L de 3 casillas";
-      }
-      else if (shapeNumber == 13){
-        size = 4;
-        shape = "L de 4 casillas";
-      }
-      else if (shapeNumber == 14){
-        size = 3;
-        shape = "L con vista hacia la izquierda de 3 casillas";
-      }
-      else if (shapeNumber == 15){
-        size = 4;
-        shape = "L con vista hacia la izquierda de 4 casillas";
-      }
-      else if (shapeNumber == 16){
-        size = 3;
-        shape = "L invertida de 3 casillas";//capaz y se pueda pensar unos mejores nombres para esto.
-      }
-      else if (shapeNumber == 17){
-        size = 4;
-        shape = "L invertida de 4 casillas";
-      }
-      else if (shapeNumber == 18){
-        size = 3;
-        shape = "L invertida con vista hacia la izquierda de 3 casillas";
-      }
-      else{
-        size = 4;
-        shape = "L invertida con vista hacia la izquierda de 4 casillas";
-      }
-
-      return shape;
+  public void shapePieces(int shapeNumber){
+    if (shapeNumber == 1){
+      this.size = 1;
+      this.type = "Square";
+    }
+    else if (shapeNumber == 2){
+      this.size = 2;//tiene 4 casillas por decirlo de una manera, esto puede funcionar para el score (si es que lo usamos asi)
+      this.type = "Square";
+    }
+    else if (shapeNumber == 3){
+      this.size = 3;
+      this.type = "Square";
+   }
+   else if (shapeNumber == 4){
+      this.size = 2;
+      this.type = "Horizontal Line";
+    }
+   else if (shapeNumber == 5){
+      this.size = 3;
+      this.type = "Horizontal Line";
+   }
+   else if (shapeNumber == 6){
+      this.size = 4;
+      this.type = "Horizontal Line";
+   }
+   else if (shapeNumber == 7){
+      this.size = 5;
+      this.type = "Horizontal Line";
+   }
+   else if (shapeNumber == 8){
+      this.size = 2;
+      this.type = "Vertical Line";
+   }
+   else if (shapeNumber == 9){
+     this.size = 3;
+     this.type = "Vertical Line";
+   }
+  else if (shapeNumber == 10){
+     this.size = 4;
+     this.type = "Vertical Line";
+   }
+   else if (shapeNumber == 11){
+     this.size = 5;
+     this.type = "Vertical Line";
+   }
+   else if (shapeNumber == 12){
+     this.size = 3;
+     this.type = "L";
+   }
+   else if (shapeNumber == 13){
+     this.size = 4;
+     this.type = "L";
+   }
+   else if (shapeNumber == 14){
+     this.size = 3;
+     this.type = "L hacia la izquierda";
+   }
+   else if (shapeNumber == 15){
+     this.size = 4;
+     this.type = "L hacia la izquierda";
+   }
+   else if (shapeNumber == 16){
+     this.size = 3;
+     this.type = "L invertida";//capaz y se pueda pensar unos mejores nombres para esto.
+   }
+   else if (shapeNumber == 17){
+     this.size = 4;
+     this.type = "L invertida";
+   }
+   else if (shapeNumber == 18){
+     this.size = 3;
+     this.type = "L invertida hacia la izquierda";
+   }
+   else if (shapeNumber == 19){
+     this.size = 4;
+     this.type = "L invertida hacia la izquierda";
+   }
+   else System.out.println("El numero es: " + shapeNumber);
   }
 
   public boolean displacement_Piece(){
